c1705da69608b308565b84d1926c4fb7bbedbf60
lilados <nefaroxispl@gmail.com>
2023-05-08 12:39:59 +0200

    added missing files

---
+++ Assets/Scripts/Scripts.Equiping/Arm.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arm : EquippableItem
+{
+   public KeyCode key = KeyCode.Alpha1;
+   public Sprite sprite;
+}
